f7d55653846b588c142084d55ed9b4d48bf5aef6
liidikone <sankariliidit@gmail.com>
2026-06-14 22:58:42 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ export default function Hero() {
       <div className="hero__content">
         <h1 className="hero__title">
           SOMESANKARIT<br />
-          →LIIDIKONE<br />
+          <span className="hero__title-glow">→LIIDIKONE</span><br />
         </h1>
         <p className="hero__lead">
           Muuta näyttökerrat rahaksi.
