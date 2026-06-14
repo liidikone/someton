62c426ea1b6078dc2b8e8cff54792cc76851400f
liidikone <sankariliidit@gmail.com>
2026-06-15 00:47:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import './styles/globals.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Features from './components/Features'
-import About from './components/About'
-import Pricing from './components/Pricing'
-import CTA from './components/CTA'
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Features />
-      <About />
-      <Pricing />
-      <CTA />
     </>
   )
 }
