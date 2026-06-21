af4e8d2badf0f4668b8f5ba69e2b4a4d11f101e5
liidikone <sankariliidit@gmail.com>
2026-06-21 12:53:28 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import '../styles/Navbar.css'
 
 const navLinks = [
@@ -10,12 +10,15 @@ const navLinks = [
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false)
   const [callOpen, setCallOpen] = useState(false)
+  const { pathname } = useLocation()
+  const isLight = pathname === '/tiimi'
 
   const toggleMenu = () => setMenuOpen(prev => !prev)
   const closeMenu  = () => setMenuOpen(false)
 
   const navClass = [
     'navbar',
+    isLight ? 'navbar--light' : '',
     menuOpen ? 'navbar--menu-open' : '',
   ].filter(Boolean).join(' ')
 
