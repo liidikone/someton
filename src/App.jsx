9a37d0d9383282e9783a43592f8400b581c6f82d
liidikone <sankariliidit@gmail.com>
2026-06-21 13:06:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import './styles/globals.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Tiimi from './components/Tiimi'
 
+function RouteBackground() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    document.documentElement.classList.toggle('bg-light', pathname !== '/')
+  }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <RouteBackground />
       <Navbar />
       <Routes>
         <Route path="/" element={<Hero />} />
