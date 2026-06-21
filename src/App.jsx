82f958d8dd173427cb9b4ac96bce27bb738c79e8
liidikone <sankariliidit@gmail.com>
2026-06-21 12:26:56 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './styles/globals.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Tiimi from './components/Tiimi'
 
 export default function App() {
   return (
