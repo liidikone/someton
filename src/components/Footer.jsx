148469fb82bab85b48b63757a88fd14773e5619b
liidikone <sankariliidit@gmail.com>
2026-06-28 21:56:54 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ export default function Footer() {
           </div>
 
           <a
-            href="https://www.tiktok.com/@someton"
+            href="https://www.tiktok.com/@sometonhq"
             target="_blank"
             rel="noopener noreferrer"
             className="footer__tiktok"
